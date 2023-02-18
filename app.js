e6a1491bd97cca9c3463b6d40e5a7efb3638a50f
kokoinovich <95966575+kokoinovich@users.noreply.github.com>
2023-02-18 16:01:05 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function solution(){
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    let c = document.getElementById('c').value;
+    document.getElementById('sum').innerText=a * b * c * 4
+}
